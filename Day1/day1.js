9d24293dc08eb84bee301c534877ce22e3840816
Eda Nur Nair <93126814+EdANaiR@users.noreply.github.com>
2023-07-18 18:46:09 +0300

    Dosyalar Localden Yüklendi

---
+++ Day1/day1.js
@@ -0,0 +1,24 @@
+//LadyDevs 30 Days Of Javascript
+
+//Veri Türü Kontrolünde 'TypeOf' Kullanılır
+//Değişken verisi değişecek ise 'let' kullanılır.
+//Değişmeyecek ise'Const' kullanılır.
+
+console.log('Hello World')
+//veriables
+
+let text='string'
+let myAge= 22
+let yourAge= 30
+let married= false
+let emptyValue
+let emptyObj= null
+
+console.log(typeof text) //string
+console.log(typeof myAge) //number
+console.log(typeof married) //boolean
+console.log(typeof emptyValue) //undefined
+console.log(typeof emptyObj) //object
+
+console.log('Ben '+myAge+' Yaşındayım')
+console.log('Sen '+yourAge+' Yaşındasın')
